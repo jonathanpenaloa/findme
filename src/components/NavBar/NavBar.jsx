82ff136c62e4cc4d94efa9d721178ad9041be471
jonathanpenaloa <jona_pena_91@outlook.com>
2023-05-31 09:57:22 -0700

    nav bar links space fix with css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -17,10 +17,8 @@ function NavBar() {
           <img src={user.avatar} alt="img" />
           <p>Hi! {user.name}</p>
           <Link to="/Book">Book</Link>
-            <div >
-              <Link to="/DashBoard">Home</Link>
-              <Link to="/About">About</Link>
-            </div>
+          <Link to="/DashBoard">Home</Link>
+          <Link to="/About">About</Link>
           <LogoutButton />
         </>
       ) : (
@@ -28,10 +26,8 @@ function NavBar() {
           <img src="https://cdn-icons-png.flaticon.com/512/1802/1802342.png" alt="img" />
           <p>Looking for a haircut?</p>
           <Link to="/Book">Book</Link>
-          <div>
-            <Link to="/About">About</Link>
-            <Link to="/Login">Log In</Link>
-          </div>
+          <Link to="/About">About</Link>
+          <Link to="/Login">Log In</Link>
         </>
       )}
     </div>
